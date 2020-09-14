43ac5ad922d5ea2e8f93b96d7d6d508425346c76
bgh269 <ovaskainenkaisa@gmail.com>
2020-09-14 13:29:01 +0300

    tapahtumat ja ravintolat navigointi tagit luotu, ravintolat componentin fetch polku vielä hakusessa

--- App.js
+++ App.js
@@ -15,67 +15,19 @@ import {
   ScrollView,
   Linking,
 } from "react-native";
+import Eventlist from "./components/eventlist";
+import Restaurants from "./components/Restaurants";
+
+const Tab = createBottomTabNavigator();
 
 function App() {
-  const [listItems, setListItems] = React.useState([]);
-  const [text, setText] = React.useState("");
-
-  const Tab = createBottomTabNavigator();
-
-  /*
-
-  function fetchData(){
-    fetch("http://open-api.myhelsinki.fi/v1/events/", {
-      method: "GET",
-      mode: "no-cors",
-      headers: {
-        "content-type": "application/json"
-      }
-    })
-    .then(responseData => responseData)
-    .then(responseData => console.log(responseData))
-  }
-
-*/
-
-  function fetchData() {
-    fetch(
-      "https://cors-anywhere.herokuapp.com/open-api.myhelsinki.fi/v1/activities/",
-      {
-        method: "GET",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          Origin: "x-requested-with",
-        },
-      }
-    )
-      .then((response) => response.json())
-      .then((responseData) => {
-        console.log(responseData.data[0]);
-        setListItems(responseData.data);
-      })
-      .catch((error) => {
-        Alert.alert("Error", error);
-      });
-  }
-
-  React.useEffect(() => {
-    fetchData();
-  }, []);
-
   return (
-    <ScrollView style={styles.HistoryContainer}>
-      {listItems.map((item) => (
-        <Text
-          onPress={() => Linking.openURL(item.info_url)}
-          style={{ fontSize: 20 }}
-          key={item.id}
-        >
-          {item.name.fi}
-        </Text>
-      ))}
-    </ScrollView>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Tapahtumat" component={Eventlist} />
+        <Tab.Screen name="Ravintolat" component={Restaurants} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
