3220c689c59dfbd53eac5aac5681670dc3304efe
HotSpor <hotspor00@gmail.com>
2020-09-14 11:42:51 +0300

    CORS kierretty erillisisen herokupalvelimen kautta TODO pitäis saada ilman toimimaan

--- App.js
+++ App.js
@@ -1,22 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import ReactTable from "react-table-v6";
-import { StyleSheet, Text, View, Button, TextInput, FlatList, Alert} from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, FlatList, Alert, ScrollView, Linking  } from 'react-native';
 
 
 function App() {
 
   const [listItems, setListItems] = React.useState([]);
-  const [text,setText] = React.useState("")
+  const [text, setText] = React.useState("")
 
+
+/*
+
+  function fetchData(){
+    fetch("http://open-api.myhelsinki.fi/v1/events/", {
+      method: "GET",
+      mode: "no-cors",
+      headers: {
+        "content-type": "application/json"
+      }
+    })
+    .then(responseData => responseData)
+    .then(responseData => console.log(responseData))
+  }
+
+*/
+
+
+  
   function fetchData() {
-    fetch('http://open-api.myhelsinki.fi/v1/activities/', {mode: "cors"})
+    fetch('https://cors-anywhere.herokuapp.com/open-api.myhelsinki.fi/v1/activities/', {
+      method: 'GET',
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Origin: "x-requested-with"
+      }
+    })
       .then(response => response.json())
       .then(responseData => {
-        console.log(responseData)
+        console.log(responseData.data[0])
         setListItems(responseData.data);
       })
-      .catch((error) => { Alert.alert('Error', error); }); 
+      .catch((error) => { Alert.alert('Error', error); });
   }
 
 
@@ -28,13 +54,21 @@ function App() {
 
 
   return (
-    <View>
-      <FlatList data={listItems} renderItem={({ item }) =>
-        <Text>{item.key}
-        </Text>}
-      />
-    </View>
+    <ScrollView style={styles.HistoryContainer}>
+      {listItems.map(item => <Text onPress={() => Linking.openURL(item.info_url)} style={{ fontSize: 20 }} key={item.id}>{item.name.fi}</Text>)}
+    </ScrollView>
+
   );
 }
+
+const styles = StyleSheet.create({
+  HistoryContainer: {
+    marginHorizontal: 20,
+    fontSize:5,
+    marginTop: 50,
+    flexDirection: "column"
+  }
+});
+
 
 export default App;
