4f7083bd1ae4c24fe290bd854e55faa5ab3f63f4
Markku Myllärinen <41078224+MarkkuMyllarinen@users.noreply.github.com>
2020-10-06 15:09:00 +0300

    Deleting merge marks

--- components/MainStackNavigator.js
+++ components/MainStackNavigator.js
@@ -11,12 +11,9 @@ const MainStackNavigator = () => {
     return (
         <Stack.Navigator >
             <Stack.Screen name="Homepage" component={Homepage} />
-<<<<<<< HEAD
             <Stack.Screen headerMode={"none"} name="EventScreen" component={EventScreen} />
-=======
             <Stack.Screen name="EventScreen" component={EventScreen} />
             <Stack.Screen name="Eventlist" component={Eventlist} />
->>>>>>> e1ee02c4a6437fa007f52e4c4eeeb70efcbe9b44
         </Stack.Navigator>
     );
 }
