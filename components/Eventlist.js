0909a55cce44e2be4687816c621dba876d315a0b
Markku <hotspor00@gmail.com>
2020-10-12 19:54:08 +0300

    Homepage tänään näppäin toimii ja vie eventlist sivulle

--- components/Eventlist.js
+++ components/Eventlist.js
@@ -1,102 +1,103 @@
-import { StatusBar } from "expo-status-bar";
+import {StatusBar} from "expo-status-bar";
 import React from "react";
 import Event from "./Event";
 import EventScreen from "./EventScreen";
 
 import {
-  StyleSheet,
-  View,
-  FlatList,
-  Alert,
-  ActivityIndicator,
+    StyleSheet,
+    View,
+    FlatList,
+    Alert,
+    ActivityIndicator,
 } from "react-native";
 
 import Search from "./Search";
-import { useLinkProps } from "@react-navigation/native";
+import {useLinkProps} from "@react-navigation/native";
 
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator} from "@react-navigation/stack";
+import {TouchableOpacity} from "react-native-gesture-handler";
 
 const EventlistStack = createStackNavigator();
 
-export default function Eventlist({ navigation, route }) {
-  const [listItems, setListItems] = React.useState([]);
-  const [listItemsKeep, setListItemsKeep] = React.useState([]);
-  const [isReady, setReady] = React.useState(false);
+export default function Eventlist({navigation, route}) {
+    const [listItems, setListItems] = React.useState([]);
+    const [listItemsKeep, setListItemsKeep] = React.useState([]);
+    const [isReady, setReady] = React.useState(true);
 
-  const { data } = route.params;
+    const {data} = route.params;
 
-  function fetchData() {
-    fetch("http://open-api.myhelsinki.fi/v1/events/", {
-      method: "GET",
-    })
-      .then((response) => response.json())
-      .then((responseData) => {
-        const sortedEvents = responseData.data.sort(function (a, b) {
-          return a.event_dates.starting_day < b.event_dates.starting_day
-            ? -1
-            : a.event_dates.starting_day > b.event_dates.starting_day
-            ? 1
-            : 0;
-        });
 
-        let today = new Date().toISOString();
+    function fetchData() {
+        fetch("http://open-api.myhelsinki.fi/v1/events/", {
+            method: "GET",
+        })
+            .then((response) => response.json())
+            .then((responseData) => {
+                const sortedEvents = responseData.data.sort(function (a, b) {
+                    return a.event_dates.starting_day < b.event_dates.starting_day
+                        ? -1
+                        : a.event_dates.starting_day > b.event_dates.starting_day
+                            ? 1
+                            : 0;
+                });
 
-        const filterDates = sortedEvents.filter(function (a) {
-          return a.event_dates.starting_day >= today;
-        });
+                let today = new Date().toISOString();
 
-        setListItems(filterDates);
-        setListItemsKeep(filterDates);
-        setReady(true);
-      })
-      .catch((error) => {
-        Alert.alert("Error", error);
-      });
-  }
+                const filterDates = sortedEvents.filter(function (a) {
+                    return a.event_dates.starting_day >= today;
+                });
 
-  function callBackFunction(newData) {
-    setListItems(newData);
-  }
+                setListItems(filterDates);
+                setListItemsKeep(filterDates);
+                setReady(true);
+            })
+            .catch((error) => {
+                Alert.alert("Error", error);
+            });
+    }
 
-  React.useEffect(() => {
-    setListItems(data)
-    setListItemsKeep(data)
-  }, []);
+    function callBackFunction(newData) {
+        setListItems(newData);
+    }
 
-  const renderItem = (item) => {
-    return <Event item={item} />;
-  };
+    React.useEffect(() => {
+        setListItems(data)
+        setListItemsKeep(data)
+    }, []);
 
-  if (!isReady) {
+    const renderItem = (item) => {
+        return <Event item={item}/>;
+    };
+
+    if (!isReady) {
+        return (
+            <View style={styles.EventListContainer}>
+                <Search keepLista={listItemsKeep} parentCallback={callBackFunction}/>
+                <ActivityIndicator style={styles.ActivityIndicator} size="large"/>
+            </View>
+        );
+    }
+
     return (
-      <View style={styles.EventListContainer}>
-        <Search keepLista={listItemsKeep} parentCallback={callBackFunction} />
-        <ActivityIndicator style={styles.ActivityIndicator} size="large" />
-      </View>
+        <View style={styles.EventListContainer}>
+            <Search keepLista={listItemsKeep} parentCallback={callBackFunction}/>
+            <FlatList
+                style={{marginLeft: 10}}
+                data={listItems}
+                keyExtractor={(item) => item.id}
+                renderItem={({item}) => renderItem(item)}
+            ></FlatList>
+        </View>
     );
-  }
-
-  return (
-    <View style={styles.EventListContainer}>
-      <Search keepLista={listItemsKeep} parentCallback={callBackFunction} />
-      <FlatList
-        style={{ marginLeft: 10 }}
-        data={listItems}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => renderItem(item)}
-      ></FlatList>
-    </View>
-  );
 }
 
 const styles = StyleSheet.create({
-  EventListContainer: {
-    flex: 1,
-    marginTop: 30,
-  },
-  ActivityIndicator: {
-    flex: 1,
-  },
+    EventListContainer: {
+        flex: 1,
+        marginTop: 30,
+    },
+    ActivityIndicator: {
+        flex: 1,
+    },
 });
--- components/Homepage.js
+++ components/Homepage.js
@@ -1,155 +1,170 @@
-import { StatusBar } from "expo-status-bar";
+import {StatusBar} from "expo-status-bar";
 import React from "react";
 import Event from "./Event";
 import DateTimePicker from '@react-native-community/datetimepicker';
 import Search from "./Search";
 import moment from "moment";
-import { useEffect } from "react";
+import {useEffect} from "react";
 
 import {
-  StyleSheet,
-  Text,
-  View,
-  Button,
-  Alert,
-  ActivityIndicator,
+    StyleSheet,
+    Text,
+    View,
+    Button,
+    Alert,
+    ActivityIndicator,
 } from "react-native";
 
 
-export default function Homepage({ navigation }) {
-  const [listItems, setListItems] = React.useState([]);
-  const [listItemsKeep, setListItemsKeep] = React.useState([]);
-  const [isReady, setReady] = React.useState(false);
-  const [date, setDate] = React.useState(new Date(1598051730000));
-  const [mode, setMode] = React.useState('date');
-  const [show, setShow] = React.useState(false);
+export default function Homepage({navigation}) {
+    const [listItems, setListItems] = React.useState([]);
+    const [listItemsKeep, setListItemsKeep] = React.useState([]);
+    const [isReady, setReady] = React.useState(false);
+    const [date, setDate] = React.useState(new Date(1598051730000));
+    const [mode, setMode] = React.useState('date');
+    const [show, setShow] = React.useState(false);
 
-  useEffect(() => {
-    fetchData()
-  }, [])
+    useEffect(() => {
+        fetchData()
+    }, [])
 
-  // Datepickerin constit
-  const onChange = (event, selectedDate) => {
-    const currentDate = selectedDate || date;
-    //setShow(Platform.OS === 'ios');
-    setDate(currentDate);
-  };
+    // Datepickerin constit
+    const onChange = (event, selectedDate) => {
+        const currentDate = selectedDate || date;
+        //setShow(Platform.OS === 'ios');
+        setDate(currentDate);
+    };
 
-  const showMode = (currentMode) => {
-    setShow(true);
-    setMode(currentMode);
-  };
+    const showMode = (currentMode) => {
+        setShow(true);
+        setMode(currentMode);
+    };
 
-  const showDatepicker = () => {
-    showMode('date');
-  };
-  
-  async function getEventsToday() {
-    let now = new Date()
-    let nowString = now.toISOString()
-    let nextMidnight = moment( moment().format('YYYY-MM-DD') + ' 23:59:00' ).unix()
-    let eventsToday = await listItems.filter( event => event.event_dates.starting_day >= nowString).filter( event =>
-      event.event_dates.starting_day <= nextMidnight)
-     // setListItemsKeep(eventsToday)
-      navigation.navigate("Eventlist", { data: eventsToday})
-  }
+    const showDatepicker = () => {
+        showMode('date');
+    };
 
-  function fetchData() {
-    fetch("http://open-api.myhelsinki.fi/v1/events/", {
-      method: "GET",
-    })
-      .then((response) => response.json())
-      .then((responseData) => {
-        const sortedEvents = responseData.data.sort(function (a, b) {
-          return a.event_dates.starting_day < b.event_dates.starting_day
-            ? -1
-            : a.event_dates.starting_day > b.event_dates.starting_day
-            ? 1
-            : 0;
-        });
+    function getEventsToday() {
 
-        let today = new Date().toISOString();
+        let nextMidnight = moment( moment().add(2,"days").format('YYYY-MM-DD') + ' 23:59:00' ).toISOString()
 
-        const filterDates = sortedEvents.filter(function (a) {
-          return a.event_dates.starting_day >= today;
-        });
+        // jos haluaa midnight ajan oikein ottaa ton .add(2,"days") pois tuolta. Lisää 2 päivää siihen aikaan jotta helpompi debugaa.
+        // kellonajat ovat UTC ajassa joten ovat 3 h jäljessä suomen aikaan.
 
-        setListItems(filterDates);
-        setListItemsKeep(filterDates);
-        setReady(true);
-      })
-      .catch((error) => {
-        Alert.alert("Error", error);
-      });
-  }
+        // data menee nytten kivasti eventlista sivulle.
 
-  function callBackFunction(newData) {
-    setListItems(newData);
-  }
+        //BUG !! tapahtumat eivät tule järjestyksessä eventlist pagelle. Viimeisimmät elementit saattavat heitellä ajassaan
 
-  React.useEffect(() => {
-    fetchData();
-  }, []);
+        // TODO selvitä ovatko eventtien ajat UTC ajassa ja pitäisikö nextMidnight ja momentTime olla UTC ajassa vai miten
 
-  const renderItem = (item) => {
-    return <Event navigation={props.navigation} item={item} />;
-  };
+        let momentTime = moment(moment().format()).toISOString()
 
-  if (!isReady) {
+
+        console.log(momentTime)
+        console.log(nextMidnight)
+
+        let eventsToday = listItems.filter(event => event.event_dates.starting_day >= momentTime).filter(event =>
+            event.event_dates.starting_day <= nextMidnight)
+        // setListItemsKeep(eventsToday)
+        navigation.navigate("Eventlist", {data: eventsToday})
+    }
+
+    function fetchData() {
+        fetch("http://open-api.myhelsinki.fi/v1/events/", {
+            method: "GET",
+        })
+            .then((response) => response.json())
+            .then((responseData) => {
+                const sortedEvents = responseData.data.sort(function (a, b) {
+                    return a.event_dates.starting_day < b.event_dates.starting_day
+                        ? -1
+                        : a.event_dates.starting_day > b.event_dates.starting_day
+                            ? 1
+                            : 0;
+                });
+
+                let today = new Date().toISOString();
+
+                const filterDates = sortedEvents.filter(function (a) {
+                    return a.event_dates.starting_day >= today;
+                });
+
+                setListItems(filterDates);
+                setListItemsKeep(filterDates);
+                setReady(true);
+            })
+            .catch((error) => {
+                Alert.alert("Error", error);
+            });
+    }
+
+    function callBackFunction(newData) {
+        setListItems(newData);
+    }
+
+    React.useEffect(() => {
+        fetchData();
+    }, []);
+
+    const renderItem = (item) => {
+        return <Event navigation={props.navigation} item={item}/>;
+    };
+
+    if (!isReady) {
+        return (
+            <View style={styles.HomepageContainer}>
+                <Search keepLista={listItemsKeep} parentCallback={callBackFunction}/>
+                <ActivityIndicator style={styles.ActivityIndicator} size="large"/>
+            </View>
+        );
+    }
+
     return (
-      <View style={styles.HomepageContainer}>
-        <Search keepLista={listItemsKeep} parentCallback={callBackFunction} />
-        <ActivityIndicator style={styles.ActivityIndicator} size="large" />
-      </View>
+        <View style={styles.HomepageContainer}>
+            <Text style={styles.TextContainer}> Löydä tapahtumat </Text>
+            <Search keepLista={listItemsKeep} parentCallback={callBackFunction}/>
+            <Text style={{marginLeft: 10}}>Päivämäärä</Text>
+            <View style={styles.Buttons}>
+                <Button
+                    title="Tänään"
+                    onPress={_ => getEventsToday()}
+                />
+                <Button
+                    title="Huomenna"
+                    onPress={() => navigation.navigate("Eventlist", {data: listItems})}
+                />
+                <Button onPress={showDatepicker} title="Valitse pvm"/>
+                {show && (
+                    <DateTimePicker
+                        testID="dateTimePicker"
+                        value={date}
+                        mode={mode}
+                        is24Hour={true}
+                        display="default"
+                        onChange={onChange}
+                    />
+                )}
+            </View>
+        </View>
     );
-  }
-
-  return (
-    <View style={styles.HomepageContainer}>
-      <Text style={styles.TextContainer}> Löydä tapahtumat </Text>
-      <Search keepLista={listItemsKeep} parentCallback={callBackFunction} />
-      <Text style={{ marginLeft: 10 }}>Päivämäärä</Text>
-      <View style={styles.Buttons}>
-        <Button
-          title="Tänään"
-          onPress={_ => getEventsToday()}
-        />
-        <Button
-          title="Huomenna"
-          onPress={() => navigation.navigate("Eventlist", { data: listItems })}
-        />
-        <Button onPress={showDatepicker} title="Valitse pvm" />
-        {show && (
-        <DateTimePicker
-          testID="dateTimePicker"
-          value={date}
-          mode={mode}
-          is24Hour={true}
-          display="default"
-          onChange={onChange} 
-        />
-        )}
-      </View>
-    </View>
-  );
 }
 
 const styles = StyleSheet.create({
-  HomepageContainer: {
-    flex: 1,
-    marginTop: 30,
-  },
-  ActivityIndicator: {
-    flex: 1,
-  },
-  TextContainer: {
-    fontSize: 30,
-    fontWeight: "bold",
-    marginBottom: 20,
-    marginLeft: 10,
-  },
-  Buttons: {
-    flexDirection: "row",
-    marginLeft: 10,
-  },
+    HomepageContainer: {
+        flex: 1,
+        marginTop: 30,
+    },
+    ActivityIndicator: {
+        flex: 1,
+    },
+    TextContainer: {
+        fontSize: 30,
+        fontWeight: "bold",
+        marginBottom: 20,
+        marginLeft: 10,
+    },
+    Buttons: {
+        flexDirection: "row",
+        marginLeft: 10,
+    },
 });
