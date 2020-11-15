ea3cd6d6eb567a859d106e71aec416991d028741
HotSpor <hotspor00@gmail.com>
2020-11-15 17:45:33 +0200

    Ravintoloiden aukioloajat toimii, uusi RestaurantCard component listätty

---
+++ components/RestaurantCard.js
@@ -0,0 +1,200 @@
+import { StatusBar } from "expo-status-bar";
+import React, { Component } from "react";
+//import ReactTable from "react-table-v6";
+import { useNavigation } from "@react-navigation/native";
+import { getDistance, orderByDistance } from "geolib";
+import { Ionicons } from "@expo/vector-icons";
+import { Icon } from "react-native-elements";
+
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    Button,
+    TextInput,
+    FlatList,
+    Alert,
+    ScrollView,
+    Linking,
+    Modal,
+    TouchableHighlight
+} from "react-native";
+
+export default function RestaurantCard(props) {
+
+    const [modalVisible, setModalVisible] = React.useState(false);
+    let item = props.item
+    let eventCoords = props.eventCoords
+
+
+    React.useEffect(() => {
+    }, [])
+
+    //const { propsItem } = route.params;
+    const orderByDistance = () => {
+        orderByDistance(eventCoords, {
+            latitude: item.location.lat,
+            longitude: item.location.lon,
+        });
+    };
+
+    let imageIcon;
+    if (item.description.images.length > 0) {
+        imageIcon = { uri: item.description.images[0].url };
+    } else {
+        imageIcon = {
+            uri:
+                "https://cdn.pixabay.com/photo/2016/09/13/18/38/silverware-1667988_1280.png",
+        };
+    }
+
+
+
+    //tulostaa 1 maanantain kohdalla, su on 0
+
+    let day = new Date().getDay();
+    console.log(day + " päivä")
+    let dayArr;
+    if (day === 0) {
+        dayArr = 6;
+    } else {
+        dayArr = day - 1;
+    }
+
+    let openingHours;
+
+    if (item.opening_hours.hours[dayArr].opens === null) {
+        openingHours = "Suljettu";
+    } else {
+        openingHours =
+            "Aukiolo tänään: " +
+            item.opening_hours.hours[dayArr].opens.substring(0, 5) +
+            " - " +
+            item.opening_hours.hours[dayArr].closes.substring(0, 5);
+    }
+
+
+    const weekdays = ["ma", "ti", "ke", "to", "pe", "la", "su"];
+    //i:n pitäisi palauttaa opening_hours arraysta jonopaikan, arvot 0-6
+
+    let hourMsg = [];
+    for (let i = 0; i < weekdays.length; i++) {
+        hourMsg.push(weekdays[i])
+        if (item.opening_hours.hours[i].opens === null) {
+            hourMsg.push("Suljettu \n");
+        } else {
+            hourMsg.push(
+                item.opening_hours.hours[i].opens.substring(0, 5) +
+                " - " +
+                item.opening_hours.hours[i].closes.substring(0, 5) +
+                "\n"
+            );
+        }
+    }
+
+
+    const alertHours = (item) => {
+        console.log(item)
+        setModalVisible(true)
+    };
+
+    return (
+        { orderByDistance },
+        (
+
+            <View style={styles.listItem}>
+
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={modalVisible}
+                    onRequestClose={() => {
+                        Alert.alert("Modal has been closed.");
+                    }}
+                >
+                    <View style={styles.centeredView}>
+                        <View style={styles.modalView}>
+                            <Text style={styles.modalText}>{hourMsg}</Text>
+
+                            <TouchableHighlight
+                                style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
+                                onPress={() => {
+                                    setModalVisible(!modalVisible);
+                                }}
+                            >
+                                <Text style={styles.textStyle}>Hide Modal</Text>
+                            </TouchableHighlight>
+                        </View>
+                    </View>
+                </Modal>
+                <View
+                    style={{ marginRight: 10, marginLeft: 10, alignItem: "center" }}
+                >
+                    <Image
+                        style={{ width: 65, height: 65 }}
+                        source={imageIcon}
+                    />
+                </View>
+                <View>
+
+                    <Text
+                        onPress={() => Linking.openURL(item.info_url)}
+                        style={{ fontSize: 15, fontWeight: "bold" }}
+                    >
+                        {item.name.fi}
+                    </Text>
+                    <Text>{item.location.address.street_address}</Text>
+                    <Text>
+                        Etäisyys:{" "}
+                        {getDistance(eventCoords, {
+                            latitude: item.location.lat,
+                            longitude: item.location.lon,
+                        })}{" "}
+            m
+          </Text>
+                    <Text>{openingHours}</Text>
+                    <Icon
+                        type="ionicon"
+                        name="md-add-circle-outline"
+                        size={20}
+                        onPress={item => alertHours(item)}
+                        style={{ flexDirection: "row", justifyContent: "flex-start" }}
+                    />
+                </View>
+            </View>
+        )
+    );
+}
+
+const styles = StyleSheet.create({
+    listItem: {
+        flex: 1,
+        marginHorizontal: 10,
+        marginVertical: 5,
+        flexDirection: "row",
+        borderWidth: 1,
+        //padding: 5,
+        alignItems: "center",
+    }, modalView: {
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5
+    },
+    openButton: {
+        backgroundColor: "#F194FF",
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2
+    },
+});
--- components/Restaurants.js
+++ components/Restaurants.js
@@ -17,17 +17,20 @@ import {
   Alert,
   ScrollView,
   Linking,
+  Modal,
+  TouchableHighlight
 } from "react-native";
+import RestaurantCard from "./RestaurantCard";
 
 export default function Restaurants({ navigation, route }) {
   const { propsItem } = route.params;
+  let proppiItem = propsItem
 
   const [listItems, setListItems] = React.useState([]);
   const [ravintolaLista, setRavintolaLista] = React.useState([]);
   const [text, setText] = React.useState("");
 
-  const distanceFilter =
-    propsItem.item.location.lat + "," + propsItem.item.location.lon + "," + 2;
+  const distanceFilter = propsItem.item.location.lat + "," + propsItem.item.location.lon + "," + 2;
 
   let eventCoords = {
     latitude: propsItem.item.location.lat,
@@ -37,7 +40,7 @@ export default function Restaurants({ navigation, route }) {
   async function fetchData() {
     fetch(
       "http://open-api.myhelsinki.fi/v1/places/?tags_search=Restaurant&distance_filter=" +
-        distanceFilter
+      distanceFilter
     )
       .then((response) => response.json())
       .then((responseData) => {
@@ -51,100 +54,7 @@ export default function Restaurants({ navigation, route }) {
   //OSAA LASKEA ETÄISYYDEN VERTAILUKOHDAKSI TÄYTYY VIELÄ MUUTTAA RAVINTOLA
   //JA FILTERÖIDÄ ETÄISYYDEN MUKAAN 10 LÄHINTÄ
 
-  const orderByDistance = () => {
-    orderByDistance(eventCoords, {
-      latitude: item.location.lat,
-      longitude: item.location.lon,
-    });
-  };
 
-  let imageIcon;
-  const iconPhoto = (item) => {
-    if (item.description.images.length > 0) {
-      imageIcon = { uri: item.description.images[0].url };
-    } else {
-      imageIcon = {
-        uri:
-          "https://cdn.pixabay.com/photo/2016/09/13/18/38/silverware-1667988_1280.png",
-      };
-    }
-  };
-  //tulostaa 1 maanantain kohdalla, su on 0
-
-  let day = new Date().getDay();
-  let dayArr;
-  if (day === 0) {
-    dayArr = 7;
-  } else {
-    dayArr = day - 1;
-  }
-
-  let openingHours;
-  const hours = (item) => {
-    if (item.opening_hours.hours[dayArr].opens === null) {
-      openingHours = "Suljettu";
-    } else {
-      openingHours =
-        "Aukiolo tänään: " +
-        item.opening_hours.hours[dayArr].opens.substring(0, 5) +
-        " - " +
-        item.opening_hours.hours[dayArr].closes.substring(0, 5);
-    }
-  };
-  /*
-  const message = (item) => {
-
-    " \n Ma " +
-      item.opening_hours.hours[0].opens.substring(0, 5) +
-      " - " +
-      item.opening_hours.hours[0].closes.substring(0, 5) +
-      "\n Ti " +
-      item.opening_hours.hours[1].opens.substring(0, 5) +
-      " - " +
-      item.opening_hours.hours[1].closes.substring(0, 5) +
-      "\n Ke " +
-      item.opening_hours.hours[2].opens.substring(0, 5) +
-      " - " +
-      item.opening_hours.hours[2].closes.substring(0, 5) +
-      "\n To " +
-      item.opening_hours.hours[3].opens.substring(0, 5) +
-      " - " +
-      item.opening_hours.hours[3].closes.substring(0, 5) +
-      "\n Pe " +
-      item.opening_hours.hours[4].opens.substring(0, 5) +
-      " - " +
-      item.opening_hours.hours[4].closes.substring(0, 5) +
-      "\n La " +
-      item.opening_hours.hours[5].opens.substring(0, 5) +
-      " - " +
-      item.opening_hours.hours[5].closes.substring(0, 5) +
-      "\n Su " +
-      item.opening_hours.hours[6].opens.substring(0, 5) +
-      " - " +
-      item.opening_hours.hours[6].closes.substring(0, 5);
-  };
-*/
-  const weekdays = ["ma", "ti", "ke", "to", "pe", "la", "su"];
-  //i:n pitäisi palauttaa opening_hours arraysta jonopaikan, arvot 0-6
-  let hourMsg = [];
-  const hourM = (item) => {
-    for (i = 0; i < weekdays.length; i++) {
-      if (item.opening_hours.hours[i].opens === null) {
-        hourMsg.push("Suljettu \n");
-      } else {
-        hourMsg.push(
-          item.opening_hours.hours[i].opens.substring(0, 5) +
-            " - " +
-            item.opening_hours.hours[i].closes.substring(0, 5) +
-            "\n"
-        );
-      }
-    }
-  }; //Tulostaa ravintoloiden aukioloajat peräkkäin
-
-  const alertHours = (item) => {
-    Alert.alert("Aukiolot");
-  };
 
   React.useEffect(() => {
     fetchData();
@@ -159,54 +69,9 @@ export default function Restaurants({ navigation, route }) {
   };
 
   const renderItem = (item) => {
-    iconPhoto(item);
-    hours(item);
-    hourM(item);
-    //message(item);
-    //alertHours(item);
-
     return (
-      { orderByDistance },
-      (
-        <View style={styles.listItem}>
-          <View
-            style={{ marginRight: 10, marginLeft: 10, alignItem: "center" }}
-          >
-            <Image
-              progressiveRenderingEnabled={true}
-              style={{ width: 65, height: 65 }}
-              source={imageIcon}
-            />
-          </View>
-          <View>
-            <Text>{hourMsg}</Text>
-            <Text
-              onPress={() => Linking.openURL(item.info_url)}
-              style={{ fontSize: 15, fontWeight: "bold" }}
-            >
-              {item.name.fi}
-            </Text>
-            <Text>{item.location.address.street_address}</Text>
-            <Text>
-              Etäisyys:{" "}
-              {getDistance(eventCoords, {
-                latitude: item.location.lat,
-                longitude: item.location.lon,
-              })}{" "}
-              m
-            </Text>
-            <Text>{openingHours}</Text>
-            <Icon
-              type="ionicon"
-              name="md-add-circle-outline"
-              size={20}
-              onPress={alertHours(item)}
-              style={{ flexDirection: "row", justifyContent: "flex-start" }}
-            />
-          </View>
-        </View>
-      )
-    );
+      <RestaurantCard item={item} eventCoords={eventCoords}></RestaurantCard>
+    )
   };
   //
   //
@@ -233,6 +98,26 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     //padding: 5,
     alignItems: "center",
+  }, modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
+  },
+  openButton: {
+    backgroundColor: "#F194FF",
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
   },
 });
 
