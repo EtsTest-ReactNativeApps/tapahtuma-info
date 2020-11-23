f3264717a1735b7af177f4a24d9163dd3617298a
HotSpor <hotspor00@gmail.com>
2020-11-23 15:21:55 +0200

    Multiple markers näkyy

--- components/RestausrantMap.js
+++ components/RestausrantMap.js
@@ -1,8 +1,8 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import MapView, { Marker } from "react-native-maps";
 import { useNavigation } from "@react-navigation/native";
-
+import { Icon } from 'react-native-elements'
 import { StyleSheet, Text, View } from "react-native";
 
 export default function EventMapScreen({ navigation, route }) {
@@ -14,10 +14,9 @@ export default function EventMapScreen({ navigation, route }) {
     {
       latitude: 60.169587,
       longitude: 24.938201,
-      latitudeDelta: 0.5,
-      longitudeDelta: 0.5,
-    },
-    []
+      latitudeDelta: 0.3,
+      longitudeDelta: 0.3,
+    }
   );
 
   let fetchRestaurant = [];
@@ -28,24 +27,39 @@ export default function EventMapScreen({ navigation, route }) {
   }
   // console.log(fetchRestaurant);
   //Ei toimi vielä oikein
+
+  useEffect(() => {
+    fetchCoordinates();
+  }, [])
+
+
   const fetchCoordinates = () => {
     fetch(
       "https://www.mapquestapi.com/geocoding/v1/batch?&inFormat=kvp&outFormat=json&thumbMaps=false&maxResults=1&location=" +
-        eventCoordsMap +
-        fetchRestaurant +
-        "&key=8oxL5Ltp3U33rpNEe7Rqbc47hfQDafLT"
+      eventCoordsMap +
+      fetchRestaurant +
+      "&key=8oxL5Ltp3U33rpNEe7Rqbc47hfQDafLT"
     )
       .then((res) => res.json())
-      .then((data) => setRegions(data));
+      .then((data) => {
+        setRegions(data);
+        setRegion({
+          latitude: data.results[0].locations[0].latLng.lat,
+          longitude: data.results[0].locations[0].latLng.lng,
+          latitudeDelta: 0.05,
+          longitudeDelta: 0.05,
+        })
+      })
   };
-  console.log(regions);
+  console.log(regions, "Regions");
 
-  let mapMarkers = Object.keys(regions).map((marker, index) => (
-    <Marker key={index} coordinate={marker.results.locations.latlng} />
-  ));
+  // let mapMarkers = Object.keys(regions).map((marker, index) => {
+  //   console.log(marker,index)
+  //   return <Marker key={index} coordinate={marker.results.locations.latlng} />
+  // });
 
   /**
-setRegion({
+  setRegion({
           latitude: data.results[0].locations[0].latLng.lat,
           longitude: data.results[0].locations[0].latLng.lng,
           latitudeDelta: 0.01,
@@ -64,20 +78,38 @@ setRegion({
           <Marker key={index} coordinate={marker.latlng} />
         ))}
         />*/
-  return (
-    <View style={styles.EventMapContainer}>
-      <MapView
-        onMapReady={fetchCoordinates}
-        style={{ flex: 6 }}
-        region={region}
-      >
-        {mapMarkers}
-      </MapView>
-      <View style={styles.container}>
-        <StatusBar style="auto" />
+
+  if (regions.length === 0) {
+    return <Text>Loading...</Text>
+  } else {
+
+    return (
+      <View style={styles.EventMapContainer}>
+        <MapView
+          style={{ flex: 6 }}
+          region={region}
+        >
+          {regions.results.slice(1, regions.length).map((marker, index) => {
+            console.log(marker, index)
+            return <Marker
+              key={index}
+              image={require('../assets/baseline_restaurant_menu_black_48dp.png')}
+              coordinate={{ latitude: marker.locations[0].latLng.lat, longitude: marker.locations[0].latLng.lng }}
+              title={marker.locations[0].street}
+            />
+          })}
+          <Marker
+            key={230}
+            coordinate={{ latitude: regions.results[0].locations[0].latLng.lat, longitude: regions.results[0].locations[0].latLng.lng }}
+            title="Tapahtuma"
+          />
+        </MapView>
+        <View style={styles.container}>
+          <StatusBar style="auto" />
+        </View>
       </View>
-    </View>
-  );
+    );
+  }
 }
 const styles = StyleSheet.create({
   EventMapContainer: {
